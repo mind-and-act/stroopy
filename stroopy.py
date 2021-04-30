4f72f6e987fbc909d02d7613aaf1cfb548d693df
Rodrigo BALP <balpeaux@gmail.com>
2021-04-30 13:54:35 +0200

    Instructions based on config files. Removed handler. Task OK.
    Signed-off-by: Rodrigo BALP <balpeaux@gamail.com>

--- stroopy.py
+++ stroopy.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
 from psychopy import event, core, data, gui, visual
-from fileHandling import *
+
+DATA_DIR = 'data'
+TYPE_TEST = 'test'
+TYPE_END = 'end'
+TYPE_PRACTICE = 'practice'
+TRIALS_PRACTICE_FILE = 'practice_list.csv'
+TRIALS_TEST_FILE = 'stimuli_list.csv'
 
 
 # noinspection PyDefaultArgument
-class Experiment:
+class StroopExperiment:
+
     def __init__(self, win_color):
         self.stimuli_positions = [[-.2, 0], [.2, 0], [0, 0]]
         self.win_color = win_color
@@ -16,14 +24,12 @@ class Experiment:
 
     @staticmethod
     def settings():
-        experiment_info = {'Subid': '', 'Age': '', 'Experiment Version': 0.1,
-                           'Sex': ['Male', 'Female', 'Other'],
-                           'Language': ['Swedish', 'English'], u'date':
-                               data.getDateStr(format="%Y-%m-%d_%H:%M")}
+        import os
+        experiment_info = {'suid': 1, 'age': 1, 'exp_version': 0.1, 'sex': ['f', 'm'], 'language': ['français', 'english'], u'date': data.getDateStr(format="%Y-%m-%d_%H:%M")}
 
-        info_dialog = gui.DlgFromDict(title='Stroop task', dictionary=experiment_info,
-                                      fixed=['Experiment Version'])
-        experiment_info[u'DataFile'] = u'Data' + os.path.sep + u'stroop.csv'
+        info_dialog = gui.DlgFromDict(title='Stroop task', dictionary=experiment_info, fixed=['exp_version'])
+
+        experiment_info[u'data_file'] = DATA_DIR + os.path.sep + f'stroop_{experiment_info["age"]:02d}.csv'
 
         if info_dialog.OK:
             return experiment_info
@@ -47,132 +53,114 @@ class Experiment:
         :rtype:
         """
 
-        text_stimuli = visual.TextStim(win=window, ori=0, name=name,
-                                       text=text, font=u'Arial',
-                                       pos=pos,
-                                       color=color, colorSpace=u'rgb')
+        text_stimuli = visual.TextStim(win=window, ori=0, name=name, text=text, font=u'Arial', pos=pos, color=color, colorSpace=u'rgb')
         return text_stimuli
 
     @staticmethod
-    def create_trials(trial_file):
-        data_types = ['Response', 'Accuracy', 'RT', 'Sub_id', 'Sex']
-        with open(trial_file, 'r') as stimfile:
-            _stims = csv.DictReader(stimfile)
-            trialz = data.TrialHandler(list(_stims), 1,
-                                       method="random")
+    def create_trials(pract=False, shuffle=True):
+        zeTrials = pd.read_csv(TRIALS_PRACTICE_FILE if pract else TRIALS_TEST_FILE)
+        zeTrials.index.name = 'index'
+        if not pract:  # add output columns
+            zeTrials['rt'] = 0.
+            zeTrials['response'] = ''
+            zeTrials['suid'] = int(settings['suid'])
+            zeTrials['sex'] = settings['sex']
+            zeTrials['accuracy'] = int(0)
 
-        [trialz.data.addDataType(data_type) for data_type in data_types]
-
-        return trialz
+        if shuffle:
+            shuffled = zeTrials.sample(len(zeTrials))
+            shuffled = shuffled.reset_index(drop=True)
+            shuffled.index.name = 'index'
+            return shuffled
+        return zeTrials
 
     @staticmethod
     def present_stimuli(color, text, position, stim):
         _stimulus = stim
         color = color
         position = position
-        if settings['Language'] == "Swedish":
-            text = swedish_task(text)
+
+        if settings['language'] == "français":
+            text = french_task(text)
         else:
             text = text
+
         _stimulus.pos = position
         _stimulus.setColor(color)
         _stimulus.setText(text)
+
         return _stimulus
 
     def running_experiment(self, trialz, testtype):
-        _trials = trialz
-        testtype = testtype
         timer = core.Clock()
         stimuli = [self.create_text_stimuli(window) for _ in range(4)]
-
-        for trial in _trials:
+        runTrials = trialz.copy(deep=True)
+        for i, trial in trialz.iterrows():
             # Fixation cross
-            fixation = self.present_stimuli('Black', '+', self.stimuli_positions[2],
-                                            stimuli[3])
+            fixation = self.present_stimuli('Black', '+', self.stimuli_positions[2], stimuli[3])
             fixation.draw()
             window.flip()
-            core.wait(.6)
+            POST_FIXATION_WAIT_DURATION = .6
+            core.wait(POST_FIXATION_WAIT_DURATION)
             timer.reset()
 
             # Target word
-            target = self.present_stimuli(trial['colour'], trial['stimulus'],
-                                          self.stimuli_positions[2], stimuli[0])
+            target = self.present_stimuli(trial['colour'], trial['stimulus'], self.stimuli_positions[2], stimuli[0])
             target.draw()
+
             # alt1
-            alt1 = self.present_stimuli('Black', trial['alt1'],
-                                        self.stimuli_positions[0], stimuli[1])
+            alt1 = self.present_stimuli('Black', trial['alt1'], self.stimuli_positions[0], stimuli[1])
             alt1.draw()
+
             # alt2
-            alt2 = self.present_stimuli('Black', trial['alt2'],
-                                        self.stimuli_positions[1], stimuli[2])
+            alt2 = self.present_stimuli('Black', trial['alt2'], self.stimuli_positions[1], stimuli[2])
             alt2.draw()
             window.flip()
 
             keys = event.waitKeys(keyList=['x', 'm'])
             resp_time = timer.getTime()
-            if testtype == 'practice':
+
+            if testtype == TYPE_PRACTICE:
                 if keys[0] != trial['correctresponse']:
-                    instruction_stimuli['incorrect'].draw()
+                    instructions['incorrect'].draw()
 
                 else:
-                    instruction_stimuli['right'].draw()
+                    instructions['right'].draw()
 
                 window.flip()
                 core.wait(2)
 
-            if testtype == 'test':
+            if testtype == TYPE_TEST:
                 if keys[0] == trial['correctresponse']:
-                    trial['Accuracy'] = 1
+                    runTrials.loc[i, 'accuracy'] = 1
                 else:
-                    trial['Accuracy'] = 0
+                    runTrials.loc[i, 'accuracy'] = 0
 
-                trial['RT'] = resp_time
-                trial['Response'] = keys[0]
-                trial['Sub_id'] = settings['Subid']
-                trial['Sex'] = settings['Sex']
-                write_csv(settings[u'DataFile'], trial)
+                runTrials.loc[i, 'rt'] = resp_time
+                runTrials.loc[i, 'response'] = keys[0]
 
             event.clearEvents()
-
-
-def create_instructions_dict(instr):
-    start_n_end = [w for w in instr.split() if w.endswith('START') or w.endswith('END')]
-    keys = {}
-
-    for word in start_n_end:
-        # noinspection RegExpDuplicateCharacterInClass
-        key = re.split("[END, START]", word)[0]
-
-        if key not in keys.keys():
-            keys[key] = []
-
-        if word.startswith(key):
-            keys[key].append(word)
-    return keys
-
-
-def create_instructions(input_, START_, END_):
-    instruction_text = parse_instructions(input_, START_, END_)
-    print(instruction_text)
-    text_stimuli = visual.TextStim(window, text=instruction_text, wrapWidth=1.2,
-                                   alignHoriz='center', color="Black",
-                                   alignVert='center', height=0.06)
-
-    return text_stimuli
+        return runTrials
 
 
 def display_instructions(start_instruction=''):
+    """
+    Displays the instructions.
+
+    :param start_instruction: the command
+    :type start_instruction: str
+    """
     # Display instructions
 
-    if start_instruction == 'Practice':
-        instruction_stimuli['instructions'].pos = (0.0, 0.5)
-        instruction_stimuli['instructions'].draw()
+    if start_instruction == TYPE_PRACTICE:
+        instructions['instructions'].pos = (0.0, 0.5)
+        instructions['instructions'].draw()
 
         positions = [[-.2, 0], [.2, 0], [0, 0]]
-        examples = [experiment.create_text_stimuli() for _ in positions]
+        examples = [strexp.create_text_stimuli() for _ in positions]
         example_words = ['green', 'blue', 'green']
-        if settings['Language'] == 'Swedish':
-            example_words = [swedish_task(word) for word in example_words]
+        if settings['language'] == 'français':
+            example_words = [french_task(word) for word in example_words]
 
         for i, pos in enumerate(positions):
             examples[i].pos = pos
@@ -189,61 +177,69 @@ def display_instructions(start_instruction=''):
 
         [example.draw() for example in examples]
 
-        instruction_stimuli['practice'].pos = (0.0, -0.5)
-        instruction_stimuli['practice'].draw()
+        instructions['practice'].pos = (0.0, -0.5)
+        instructions['practice'].draw()
 
-    elif start_instruction == 'Test':
-        instruction_stimuli['test'].draw()
+    elif start_instruction == TYPE_TEST:
+        instructions['test'].draw()
 
-    elif start_instruction == 'End':
-        instruction_stimuli['done'].draw()
+    elif start_instruction == TYPE_END:
+        instructions['done'].draw()
 
     window.flip()
     event.waitKeys(keyList=['space'])
     event.clearEvents()
 
 
-def swedish_task(word):
-    swedish = '+'
+def french_task(word):
+    français = '+'
     if word == "blue":
-        swedish = u"blå"
+        français = u"bleu"
 
     elif word == "red":
-        swedish = u"röd"
+        français = u"rouge"
 
     elif word == "green":
-        swedish = u"grön"
+        français = "vert"
 
     elif word == "yellow":
-        swedish = "gul"
+        français = "jaune"
 
-    return swedish
+    return français
+
+
+def read_instructions(lang):
+    global window
+    import configparser
+    configIn = configparser.ConfigParser()
+    configIn.read("instructions.ini")
+    configOut = {}
+    for ck, cv in configIn[lang].items():
+        configOut[ck] = visual.TextStim(window, text=configIn[lang][ck], wrapWidth=1.2, alignHoriz='center', color="Black", alignVert='center', height=0.06)
+    return configOut
 
 
 if __name__ == "__main__":
-    experiment = Experiment(win_color="White")
-    settings = experiment.settings()
-    language = settings['Language']
-    instructions = read_instructions_file("INSTRUCTIONS", language, language + "End")
-    instructions_dict = create_instructions_dict(instructions)
-    instruction_stimuli = {}
+    strexp = StroopExperiment(win_color="White")
+    settings = pd.Series(strexp.settings())
+    language = settings['language']
 
-    window = experiment.create_window()
+    window = strexp.create_window()
+    instructions = read_instructions(language)
 
-    for instruction, (START, END) in instructions_dict.items():
-        instruction_stimuli[instruction] = create_instructions(instructions, START, END)
     # We don't want the mouse to show:
     event.Mouse(visible=False)
-    # Practice Trials
-    display_instructions(start_instruction='Practice')
+    ##- Practice Trials
+    display_instructions(start_instruction=TYPE_PRACTICE)
+    practiceTrials = strexp.create_trials(pract=True)
+    _ = strexp.running_experiment(practiceTrials, testtype='practice')
 
-    practice = experiment.create_trials('practice_list.csv')
-    experiment.running_experiment(practice, testtype='practice')
-    # Test trials
-    display_instructions(start_instruction='Test')
-    trials = experiment.create_trials('stimuli_list.csv')
-    experiment.running_experiment(trials, testtype='test')
+    ##- Test trials
+    display_instructions(start_instruction=TYPE_TEST)
+    testTrials = strexp.create_trials()
+    run_trials = strexp.running_experiment(testTrials, testtype='test')
+    run_trials.to_csv(settings.data_file, sep='\t')
 
     # End experiment but first we display some instructions
-    display_instructions(start_instruction='End')
+    display_instructions(start_instruction='end')
     window.close()
